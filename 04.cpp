e707eafd25d6333abe963e9b39c7c5913c499099
Arka Banerjee <arkabanerjee381@gmail.com>
2024-09-06 22:09:42 +0000

    cpp

---
+++ 04.cpp
@@ -0,0 +1,12 @@
+// variables
+
+#include <iostream>
+using namespace std;
+
+int main (){
+    //declaring a varible of data-type integer 
+    int a = 12;
+    //now printing it out
+    cout << "the value of a is: "<< a <<endl;
+    return 0;
+}
