16384f751f92efafa0677e0633af33a6e652b8f0
Arka Banerjee <arkabanerjee381@gmail.com>
2024-09-06 22:54:49 +0000

    cpp

---
+++ 07.cpp
@@ -0,0 +1,17 @@
+//type casting
+//Converting the data type from one to another:
+
+//type conversion:
+// (implicit)
+// small --> big
+
+#include <iostream>
+using namespace std;
+
+int main (){
+    char m = 'A'; // one cant use "" double quotes in char namification
+    int a = char (m);
+    cout << a << endl;
+    return 0;
+}
+
