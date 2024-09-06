0bbd952d2128de2f92e68e9574ee5299c86dbbea
Arka Banerjee <arkabanerjee381@gmail.com>
2024-09-06 22:37:43 +0000

    cpp

--- 03.cpp
+++ 03.cpp
@@ -1,10 +1,25 @@
-// data types:
+/* data types:
 
-/*
+
+//PRIMITIVE DATA TYPES:
+
 in C++, there are 5 types of data types (roughly):
 int
+    (takes 4 bytes space, stores integers)
 char
-float
-bool
+    (takes 1 byte space, stores single character)
+float 
+    (take 4 bytes space, stores float numbers)
+bool   
+    (takes 1 byte, space either true or false) 
+    // unlike python it has small t & f in true and false respectively
+    cout == either 0 or 1 . 0 for false; 1 for true.
 double
+    (takes 8 bytes (the double of floats))
+    by default, the doubles are the first taken in code
+    unless on mentions F or f at the end of the value.
+
+
+// NON-PRIMITIVE DATA TYPES:
+
 */
