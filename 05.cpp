34856c817cdfc761d9f0f01e1cad19cc7b9dba57
Arka Banerjee <arkabanerjee381@gmail.com>
2024-09-06 22:15:47 +0000

    cpp

--- 05.cpp
+++ 05.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 int main(){
-    float PIE = 3.14F;// F or f must be specified otherwise it will think that the value is a double
+    float PIE = 3.14F; // F or f must be specified otherwise it will think that the value is a double
     cout << "the value of PIE (till 2 places after decimal is): "<< PIE <<endl;
     return 0;
 }
