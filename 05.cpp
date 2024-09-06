ffae7f36fc5e61ffaa9a9471bad93fd9a3e7187f
Arka Banerjee <arkabanerjee381@gmail.com>
2024-09-06 22:13:41 +0000

    cpp

---
+++ 05.cpp
@@ -0,0 +1,10 @@
+// declaring a variable with the value of a float
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    float PIE = 3.14F;// F or f must be specified otherwise it will think that the value is a double
+    cout << "the value of PIE (till 2 places after decimal is): "<< PIE <<endl;
+    return 0;
+}
