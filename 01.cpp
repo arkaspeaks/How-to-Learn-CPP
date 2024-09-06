bbfbd1c3ae005df430584ddcc405f8ecd3be378d
Arka Banerjee <arkabanerjee381@gmail.com>
2024-09-06 22:04:50 +0000

    cpp

---
+++ 01.cpp
@@ -0,0 +1,9 @@
+// boiler plate code
+
+# include <iostream>
+using namespace std;
+
+int main(){
+    //write the main code here
+    return 0;
+}
---
+++ 02.cpp
@@ -0,0 +1,9 @@
+// print statement
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "Hello World !! \n";
+    return 0;
+}
