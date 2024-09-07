716bccc45d1783da22cf0d22e62c3d4afe2c9f39
Arka Banerjee <arkabanerjee381@gmail.com>
2024-09-07 17:19:06 +0000

    cpp

---
+++ 09.cpp
@@ -0,0 +1,15 @@
+//input statement
+
+//the easier approach:
+# include <iostream>
+using namespace std;
+
+int main (){
+    int m;
+    cin>> m;
+    cout<< m << endl;
+    return 0;
+}
+
+
+// the complex one in next page
---
+++ 10.cpp
@@ -0,0 +1,12 @@
+
+// a little complex approach
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x;
+    cout<< "pls provide the value of x: ";
+    cin >> x;
+    cout<< "the value of x is: "<< x << endl;
+    return 0;
+}
