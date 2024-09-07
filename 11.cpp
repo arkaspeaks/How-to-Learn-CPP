7ac3abfacbac294621be4a55c644ff141851f342
Arka Banerjee <arkabanerjee381@gmail.com>
2024-09-07 17:25:41 +0000

    cpp

---
+++ 11.cpp
@@ -0,0 +1,16 @@
+//sum of 2 numbers
+
+#include <iostream>
+using namespace std;
+
+int main (){
+    int x;
+    int y;
+    cout << "enter the value of x: ";
+    cin >> x;
+    cout << "entee the value of y: ";
+    cin >> y;
+    int sum = (x +y);
+    cout << "the value of sum: "<< sum <<endl ;
+    return 0;
+}
